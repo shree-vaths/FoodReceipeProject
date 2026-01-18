5eb047971915cec175786493a8f6831d8198fe2e
skarawalsatishrao@hawk.iit.edu <skarawalsatishrao@hawk.iit.edu>
2026-01-18 15:03:56 -0600

    fix caching

--- app/actions.js
+++ app/actions.js
@@ -1,5 +1,6 @@
 'use server'
 import { saveMeal } from '@/lib';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
   export async function shareMeal(formData){
@@ -14,6 +15,6 @@ import { redirect } from 'next/navigation';
     };
 
     await saveMeal(meal);
-
+    revalidatePath("/meals", 'layout');
     redirect("/meals");
   }
